448c94b1286568354266b504a460f53dbd68f98c
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-04 11:02:34 +0800

    update
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- docker.sh
+++ docker.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-echo -n "welcome ggz docker image."
+output() {
+  printf "\E[0;33;40m"
+  echo $1
+  printf "\E[0m"
+}
+
+output "welcome ggz docker image."
 which aptitude || apt-get -qqy install aptitude
 aptitude -y update && aptitude -y install make git gcc libncurses5-dev
 cd ~ && git clone https://github.com/appleboy/Shell-Script.git
 cd Shell-Script && ./Ubuntu.sh -i docker
 # create deploy user
-echo -n "create deploy user"
+output "create deploy user"
 useradd -m -s /bin/bash deploy
-echo -n "append docker group to deploy user"
+output "append docker group to deploy user"
 usermod -aG docker deploy
-echo -n "append docker group to ubuntu user"
+output "append docker group to ubuntu user"
 usermod -aG docker ubuntu
 
-echo
-echo "Install Completely!!"
-echo
+output
+output "Install Completely!!"
+output
